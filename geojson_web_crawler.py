eac5545aad4c17086624a7068c51dae878ae4bb3
ChuangChenLin <b08901010@ntu.edu.tw>
2019-12-07 23:07:14 +0800

    Crawler_version2 Metro line ID crawler

--- geojson_web_crawler.py
+++ geojson_web_crawler.py
@@ -12,3 +12,21 @@ profile.set_preference('browser.download.manager.showWhenStarting', False)
 profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/json')
 # Set browser
 browser = webdriver.Firefox(executable_path = driverPath, firefox_profile=profile)
+
+### Find relations id of every metro line
+id_list = []
+
+# Access urban area lines (x9) by url_browser
+url_wiki = 'https://wiki.openstreetmap.org/wiki/Tokyo_Metro'
+browser.get(url_wiki)
+id_elem = browser.find_elements_by_class_name("plainlinks") # Plainlinks are element attributes of relations id
+for id in id_elem:
+    id_list.append(id.text)             # Text is the string attribute of relations
+
+# Access suburb area metro lines (x4) by transfer link
+browser.find_element_by_link_text('Tokyo Metropolitan Bureau of Transportation').click()
+id_elem2 = browser.find_elements_by_class_name("plainlinks") # Plainlinks are element attributes of relations id
+for id2 in id_elem2:
+    id_list.append(id2.text)            # Text is the string attribute of relations
+
+id_list = [id.lstrip() for id in id_list]
