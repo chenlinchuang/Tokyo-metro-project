78634d72aef8da70264942cf772d93272b47af8b
ChiangHaoWei <123@gmail.com>
2019-12-11 00:51:20 +0800

    osm

--- read_osm.py
+++ read_osm.py
@@ -10,22 +10,22 @@ class CounterHandler(osmium.SimpleHandler):
 
     def node(self, n):
         self.num_nodes += 1
+        if self.num_nodes == 1:
+            print(n.tags['ref'])
 
         #print(type(n))
 
     def way(self, x):
         self.way_nodes += 1
         CounterHandler.node_list.append(x)
-        if self.way_nodes == 1:
+        '''if self.way_nodes == 1:
             print(CounterHandler.node_list)
-        #print(type(x))
+        #print(type(x))'''
     
-    def __str__(self):
-        return str(CounterHandler.node_list)
 if __name__ == '__main__':
 
     h = CounterHandler()
 
     h.apply_file("export.osm")
-    print(h)
+    print(h.node_list[0].id)
     print("Number of nodes: %d" % h.num_nodes)
