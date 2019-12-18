dbb4271bba0f535623151dfee8c2200e7729e932
ChuangChenLin <b08901010@ntu.edu.tw>
2019-12-18 11:00:17 +0800

    Version5 clean data

--- find_relations.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
-driverPath = 'geckodriver.exe'
-browser = webdriver.Firefox(executable_path = driverPath)
-
-
-print(id_list())
-
--- test_pandas.py
+++ test_pandas.py
@@ -60,3 +60,37 @@ for rows in range(nodes_gdf.shape[0]):
 #    print(Point(ways_gdf.loc[rows, 'geometry']))
 #gdf.geometry = gdf.geometry.apply(lambda x: Point(list(x.exterior.coords)))
 #print(ways_gdf.head())
+
+
+
+'''
+gdf = gpd.read_file('export.geojson')
+count = 0
+nodes_index_list = []
+lines_reference_list = ['A','I','S','E','G','M','H','T','C','Y','Z','N','F']
+#print(gdf.head())
+class Relations:
+    
+    def __init__(self,id):
+        self.id = id
+        self.ref = ref
+        self.previous_node = self
+    def get_previous_node(self):
+        self.ref[1:]
+    pass
+
+for i in range(len(lines_reference_list)):
+    a = Relations(lines)
+
+for rows in range(gdf.shape[0]):
+    if gdf.loc[rows,'id'].startswith('node/') and 'H' in str(gdf.loc[rows,'ref']):
+        nodes_index_list.append(rows)
+
+for rows in range(gdf.shape[0]):
+    if gdf.loc[rows,'id'].startswith('node/') and gdf.loc[rows,'ref'].startswith('G'):
+        nodes_index_list.append(rows)
+
+nodes_gdf = gdf.loc[nodes_index_list,:]
+print(nodes_gdf.loc[:,'ref'])
+#print(nodes_gdf)
+'''
